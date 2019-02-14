3e5a85608a136929cddc3cb2d2b3cd242a823e9b
Vera <17062292@student.hhs.nl>
2019-02-14 10:03:58 +0100

    Create afstand.js

---
+++ scripts/afstand.js
@@ -0,0 +1,56 @@
+var ctx = document.getElementById("myChartAfstand").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["11/2", "12/2", "13/2", "14/2", "15/2", "16/2", "17/2"],
+        datasets: [{
+            label: 'Afstand afgelegd per dag',
+            data: [50, 45, 30, 25, 20, 15, 10],
+            backgroundColor: [
+                'rgb(0, 82, 136, 0.5)',
+                'rgb(0, 82, 136, 0.5)',
+                'rgb(0, 82, 136, 0.5)',
+                'rgb(0, 82, 136, 0.5)',
+                'rgb(0, 82, 136, 0.5)',
+                'rgb(0, 82, 136, 0.5)',
+                'rgb(0, 82, 136, 0.5)',
+
+            ],
+            borderColor: [
+                'rgb(214, 238, 255)',
+                'rgb(214, 238, 255)',
+                'rgb(214, 238, 255)',
+                'rgb(214, 238, 255)',
+                'rgb(214, 238, 255)',
+                'rgb(214, 238, 255)',
+                'rgb(214, 238, 255)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Datum'
+                }
+            }],
+                        
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'x10000 km'
+                }
+            }]
+        }
+    }
+});
+
+
+
