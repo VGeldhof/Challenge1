35f316e9240a99d04d2aeafb81a5a0e9c5f55e77
Vera <17062292@student.hhs.nl>
2019-02-14 10:04:56 +0100

    Create temperatuur.js

---
+++ scripts/temperatuur.js
@@ -0,0 +1,36 @@
+var ctx = document.getElementById("myChartTemperatuur").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ["Aantal dagen boven 0℃", "Aantal dagen onder 0℃"],
+        datasets: [{
+            label: 'Temperatuur',
+            data: [2, 5],
+            backgroundColor: [
+                'rgb(0, 85, 160, 0.5)',
+                'rgb(5, 137, 255, 0.1)',
+            ],
+            borderColor: [
+                'rgb(0, 82, 136)',
+                'rgb(0, 82, 136)',
+            ],
+            borderWidth: 1,
+            hoverBorderColor: [
+                'rgb(214, 238, 255, 0.5)',
+                'rgb(63, 111, 145, 0.5)',
+            ],
+            hoverBorderWidth: 5
+
+        }]
+    },
+    options: {
+        // cutoutPercentage: 35
+        // scales: {
+        //     yAxes: [{
+        //         ticks: {
+        //             beginAtZero:true
+        //         }
+        //     }]
+        // }
+    }
+});
