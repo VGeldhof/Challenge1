44768a99df3dc099cd62f2b19d6fabe5ae39c361
Vera <17062292@student.hhs.nl>
2019-02-18 14:45:49 +0100

    Update brandstofsnelheid.js

--- scripts/brandstofsnelheid.js
+++ scripts/brandstofsnelheid.js
@@ -1,11 +1,14 @@
 var ctx = document.getElementById("myChart").getContext('2d');
+var snelheid = [12, 12.4, 11.9, 10, 11.6, 12.1, 12.4];
+var brandstof = [10, 9, 8, 7, 5, 3, 1];
+
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: ["11/2", "12/2", "13/2", "14/2", "15/2", "16/2", "17/2"],
         datasets: [{
             label: 'Snelheid (x10000km/u)',
-            data: [12, 12.4, 11.9, 10, 11.6, 12.1, 12.4],
+            data: snelheid,
             backgroundColor: [
                 'rgb(214, 238, 255, 0.5)',
             ],
@@ -18,7 +21,7 @@ var myChart = new Chart(ctx, {
 
         {
             label: 'Brandstof (liter)',
-            data: [10, 9, 8, 7, 5, 3, 1],
+            data: brandstof,
             backgroundColor: [
                 'rgb(0, 82, 136, 0.5)',
             ],
